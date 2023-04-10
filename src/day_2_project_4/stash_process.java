d15b58fb24e58d2a255c49d85690fb9f888babbf
zaki <zaki@localhost>
2023-04-09 23:51:02 -0700

    z

---
+++ src/day_2_project_4/stash_process.java
@@ -0,0 +1,9 @@
+package day_2_project_4;
+public class stash_process {
+	public static void main(String[] args) {
+		System.out.println("Stash process 4 ");
+	}
+	public static void adj() {
+		System.out.println("Example");
+	}
+}
